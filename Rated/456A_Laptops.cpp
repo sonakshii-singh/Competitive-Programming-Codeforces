48629c1f4e7f7b0dd77733cb55f749ec07d4397d
sonakshii-singh <sonakshisinghpatel@gmail.com>
2026-09-10 23:48:11 +0530

    Solved 456A

---
+++ Rated/456A_Laptops.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> laptop(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> laptop[i].first >> laptop[i].second;
+    }
+
+    sort(laptop.begin(), laptop.end());
+
+    for (int i = 1; i < n; i++) {
+        if (laptop[i].second < laptop[i - 1].second) {
+            cout << "Happy Alex";
+            return 0;
+        }
+    }
+
+    cout << "Poor Alex";
+
+    return 0;
+}
