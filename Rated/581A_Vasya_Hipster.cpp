d690efa8fc2bdaf127da44003f360a2bdf032281
sonakshii-singh <sonakshisinghpatel@gmail.com>
2026-09-12 23:53:52 +0530

    Solved 581A

---
+++ Rated/581A_Vasya_Hipster.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int different = min(a, b);
+    int same = abs(a - b) / 2;
+
+    cout << different << " " << same;
+
+    return 0;
+}
