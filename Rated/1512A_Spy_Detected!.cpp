9db9e1aad33b50ce28eb8284fb01d415c2bdd8d4
sonakshii-singh <sonakshisinghpatel@gmail.com>
2026-09-18 23:54:05 +0530

    Solved 1512A

--- Rated/1512A_Spy_Detected!.cpp
+++ Rated/1512A_Spy_Detected!.cpp
@@ -10,24 +10,18 @@ int main() {
         cin >> n;
 
         vector<int> a(n);
+        int freq[101] = {};
 
         for (int i = 0; i < n; i++) {
             cin >> a[i];
+            freq[a[i]]++;
         }
-        if (a[0] != a[1] && a[0] != a[2])
-            cout << 1 << endl;
-        else if (a[1] != a[0] && a[1] != a[2])
-            cout << 2 << endl;
-        else {
-        
-            for (int i = 3; i < n; i++) {
-                if (a[i] != a[0]) {
-                    cout << i + 1 << endl;
-                    break;
-                }
+
+        for (int i = 0; i < n; i++) {
+            if (freq[a[i]] == 1) {
+                cout << i + 1 << endl;
+                break;
             }
         }
     }
-
-    return 0;
 }
